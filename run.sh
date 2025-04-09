84a832448f4274522e5dda0ae5e244b7623c0004
S-Slobodeaniuc <129166022+S-Slobodeaniuc@users.noreply.github.com>
2025-04-09 08:45:57 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+
+# Access the options from Home Assistant configuration
+export BROKER_IP=$(bashio::config'broker_ip')
+export BROKER_PORT=$(bashio::config'broker_port')
+export MQTT_USER=$(bashio::config 'mqtt_user')  # Get the MQTT user from config.yaml
+export MQTT_PASS=$(bashio::config 'mqtt_pass')  # Get the MQTT password from config.yaml
+
 /app/my_mqtt_app
