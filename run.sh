fba289ebc8f68dfd4d0964ddbf17d8521410c31f
S-Slobodeaniuc <129166022+S-Slobodeaniuc@users.noreply.github.com>
2025-04-10 10:48:41 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,6 +13,14 @@ export MQTT_PASS=$(bashio::config 'mqtt_pass')  # Get the MQTT password from con
 bashio::log.info "Using broker: $BROKER_IP:$BROKER_PORT"
 bashio::log.info "Using MQTT user: $MQTT_USER"
 
+# Access the configuration options directly from environment variables
+export BROKER_IP=$BROKER_IP
+export BROKER_PORT=$BROKER_PORT
+export MQTT_USER=$MQTT_USER
+export MQTT_PASS=$MQTT_PASS
+
+echo "TEST TEST TEST"
+
 # Make sure the MQTT application can be executed (path to your app depends on Docker setup)
 # Assuming your executable is located in the /app directory
-/app/my_mqtt_app
+exec /app/my_mqtt_app
